6be2c0aae7d6047e90b79f9ffe61533bb4c1fcc2
Kaleb657 <kalebeyasuseifu@gmail.com>
2025-03-12 21:19:10 +0300

    updated Progrm.cs

--- C#_training/Program.cs
+++ C#_training/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.Write("this is the first line ");
+Console.Write("this is on the same line ");
